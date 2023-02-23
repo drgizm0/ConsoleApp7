ca29d8388d6f5ae0c17ef0c42b152477ba0da6af
Admin <Admin@DESKTOP-9L5JQOO>
2023-02-23 18:56:10 +0800

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace OddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd numbers between 1 and 300 divisible by 5:\n");
+
+            for (int i = 1; i <= 300; i++)
+            {
+                if (i % 2 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
